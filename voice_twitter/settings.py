eba2b1641a2429221831f15600983a984cdf9c41
Akbar Ali <akkuakbar97@gmail.com>
2022-06-04 17:02:15 +0400

    setup: adds static_root for generating static folder

--- voice_twitter/settings.py
+++ voice_twitter/settings.py
@@ -149,6 +149,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "dashboard/static"),
     os.path.join(BASE_DIR, "sso/static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
