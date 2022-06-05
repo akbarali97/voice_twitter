2c7ccfca8119d34e94d58c26c45e7e048b94c37d
Akbar Ali <akkuakbar97@gmail.com>
2022-06-05 21:03:23 +0400

    update: Creates url and function bp to post a tweet.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, post_tweet
 
 urlpatterns = [
     path('', home, name='home'),
+    path('post_tweet', post_tweet, name='post_tweet_ajax'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
 
 @login_required
 @require_http_methods(["GET"])
 def home(request):
     context = {}
-    messages.info(request, 'You are loged in!')
     return render(request,'dashboard.html', context)
+
+@login_required
+@require_http_methods(["POST"])
+def post_tweet(request):
+    if request.is_ajax:
+        print(request.POST)
+    else:
+        return JsonResponse({"error": "Not an ajax request"}, status=400)
