68fcf480b13f12d04d0fc6d19e204729d503a3dd
Akbar Ali <akkuakbar97@gmail.com>
2022-06-06 01:14:25 +0400

    update: Adds dashboard to installed apps

--- voice_twitter/settings.py
+++ voice_twitter/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sso.apps.SsoConfig',
+    'dashboard.apps.DashboardConfig',
     'django.contrib.sites',
 
     # AllAuth sso providers
