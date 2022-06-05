c9b0d21e20eca31e7d64ec8d83bd29a9a3f67073
Akbar Ali <akkuakbar97@gmail.com>
2022-06-06 02:39:08 +0400

    update:configure urls to serve media file.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home, post_tweet
 
 urlpatterns = [
     path('', home, name='home'),
-    path('post_tweet', post_tweet, name='post_tweet_ajax'),
+    path('post_tweet', post_tweet, name='post_tweet'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
