35c613d6956d52d70e593cb46b8f3f6133cd229a
Akbar Ali <akkuakbar97@gmail.com>
2022-06-06 02:36:58 +0400

    update: Stores tweets in db.

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,17 +3,23 @@ from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
+from .models import Tweet
 
 @login_required
 @require_http_methods(["GET"])
 def home(request):
-    context = {}
+    context = {'tweets': Tweet.objects.all()}
     return render(request,'dashboard.html', context)
 
 @login_required
 @require_http_methods(["POST"])
 def post_tweet(request):
-    if request.is_ajax:
-        print(request.POST)
+    if request.FILES.get('audio-file', False):
+        uploaded_file = request.FILES['audio-file']
+        Tweet.objects.create(
+            user=request.user,
+            tweet=uploaded_file
+        )
+        return JsonResponse({"success": 'audio-file.mp3'}, status=200)
     else:
-        return JsonResponse({"error": "Not an ajax request"}, status=400)
+        return JsonResponse({"error": "base64audio not found"}, status=400)
