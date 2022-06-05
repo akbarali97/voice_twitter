3b787f5f2b372eaa7e7f7018b1ee446a23f1f645
Akbar Ali <akkuakbar97@gmail.com>
2022-06-05 15:04:37 +0400

    update: setup toasts for messages. And header fix

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 @login_required
 @require_http_methods(["GET"])
 def home(request):
     context = {}
+    messages.info(request, 'You are loged in!')
     return render(request,'dashboard.html', context)
