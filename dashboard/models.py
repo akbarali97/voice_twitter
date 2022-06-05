29479ce97d5660ef0ff3418084fe315c55d264fe
Akbar Ali <akkuakbar97@gmail.com>
2022-06-06 03:32:27 +0400

    update: Adds comments model to track commets on tweets

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Tweet(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='tweets')
     tweet = models.FileField(null=False, blank=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name='comments')
+    comment = models.FileField(null=False, blank=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
