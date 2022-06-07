dfba03ccffce25c32cd7ff57865ead8636be92d6
Akbar Ali <akkuakbar97@gmail.com>
2022-06-08 03:28:31 +0400

    update: Adds url for posting comments

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home, post_tweet
+from .views import home, post_tweet, post_comment
 
 urlpatterns = [
     path('', home, name='home'),
     path('post_tweet', post_tweet, name='post_tweet'),
+    path('post_comment', post_comment, name='post_comment'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
