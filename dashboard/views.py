8d96a09230f6323c1f7d5becf46d636d66d465e2
Akbar Ali <akkuakbar97@gmail.com>
2022-06-08 03:35:49 +0400

    update: Adds function to save comments

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,8 +3,8 @@ from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
-from .models import Tweet
-
+from .models import Tweet, Comment
+from django.core.exceptions import ObjectDoesNotExist
 @login_required
 @require_http_methods(["GET"])
 def home(request):
@@ -20,6 +20,24 @@ def post_tweet(request):
             user=request.user,
             tweet=uploaded_file
         )
-        return JsonResponse({"success": 'audio-file.mp3'}, status=200)
+        return JsonResponse({"success": 'tweet posted'}, status=200)
     else:
-        return JsonResponse({"error": "base64audio not found"}, status=400)
+        return JsonResponse({"error": "audio-file not found"}, status=404)
+
+@login_required
+@require_http_methods(["POST"])
+def post_comment(request):
+    if request.FILES.get('audio-file', False) and request.POST.get('tweet_id', False):
+        try:
+            tweet = Tweet.objects.get(id=request.POST['tweet_id'])
+        except ObjectDoesNotExist:
+            return JsonResponse({"error": "Tweet does not exist"}, status=404)
+        uploaded_file = request.FILES['audio-file']
+        Comment.objects.create(
+            user=request.user,
+            tweet=tweet,
+            comment=uploaded_file
+        )
+        return JsonResponse({"success": 'comment posted'}, status=200)
+    else:
+        return JsonResponse({"error": "Bad request. Check request body."}, status=400)
